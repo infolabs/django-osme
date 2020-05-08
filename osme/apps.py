c458eb025a39ba08b37ceb959f51463367eb6afa
Rebus <romanov@excentrics.ru>
2020-05-08 13:33:16 +0300

    Подготовка к переходу на python 3.7

--- osme/apps.py
+++ osme/apps.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class OsmeConfig(AppConfig):
     name = 'osme'
-    verbose_name = 'osme'
+    verbose_name = _('OSME')
 
     def ready(self):
         from osme.signals import handlers
