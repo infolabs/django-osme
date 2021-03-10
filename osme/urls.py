1c5e75cd352bc937452a522dfae5db39d714556c
beloslav <velislav1329@gmail.com>
2021-03-10 12:08:59 +0300

    OSK-342 переделал получение регионов

--- osme/urls.py
+++ osme/urls.py
@@ -5,5 +5,5 @@ from osme.views import regions_data_view
 
 
 urlpatterns = [
-    url(r'^v1/(?P<path>.*)$', regions_data_view, name='regions_data'),
+    url(r'^api/0.6/relation/(?P<path>\d+)/full$', regions_data_view, name='regions_data'),
 ]
--- osme/views.py
+++ osme/views.py
@@ -2,29 +2,25 @@
 
 import requests
 from django.core.cache import cache
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse
 from osme.models import Region
 
 
-REGIONS_DATA_URL = "http://data.esosedi.org/regions/v1/"
+REGIONS_DATA_URL = "https://www.openstreetmap.org/api/0.6/relation/"
 
 
 def regions_data_view(request, path):
-    if 'QUERY_STRING' not in request.META:
-        return HttpResponseBadRequest()
-
-    query_string = request.META['QUERY_STRING']
-    cache_key = Region.CACHE_KEY_PATTERN.format(query_string)
+    cache_key = Region.CACHE_KEY_PATTERN.format(path)
 
     content = cache.get(cache_key, None)
     if content is not None:
         return HttpResponse(content, content_type='text/plain')
 
     try:
-        region = Region.objects.get(query_string=query_string)
+        region = Region.objects.get(query_string=path)
     except Region.MultipleObjectsReturned:
         # delete everything and create a new one from the server
-        regions = Region.objects.filter(query_string=query_string)
+        regions = Region.objects.filter(query_string=path)
         regions.delete()
     except Region.DoesNotExist:
         pass
@@ -32,10 +28,12 @@ def regions_data_view(request, path):
         cache.set(cache_key, region.content)
         return HttpResponse(region.content, content_type='text/plain')
 
-    url = '{}{}?{}'.format(REGIONS_DATA_URL, path, query_string)
+    url = '{}{}/{}'.format(REGIONS_DATA_URL, path, "full")
     r = requests.get(url, headers={'User-Agent': "curl/7.38.0"})
     if r.status_code != requests.codes.ok:
         return HttpResponse('Server responded with error', status=r.status_code)
     else:
-        Region(query_string=query_string, content=r.content).save()
+        import osm2geojson
+        geojson = osm2geojson.xml2geojson(r.content, filter_used_refs=False, log_level='INFO')
+        Region(query_string=path, content=geojson).save()
         return HttpResponse(r.content, status=r.status_code, content_type='text/plain')
